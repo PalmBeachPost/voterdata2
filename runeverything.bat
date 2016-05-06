4625d3fdace281a5b992d7ae96a8795c48e0a292
Mike Stucka <stucka@github>
2016-05-06 15:27:25 -0400

    Parallelization

--- runeverything.bat
+++ runeverything.bat
@@ -1,15 +1,13 @@
 rem All voter extract data should be in folder VoterExtract
+
+start c:\pypy\pypy voterhistory.py
+
 c:\pypy\pypy voters.py
 
-
 rem call pandame.bat
-csvgrep -c county -m Pal bigfile.csv >rawpal.csv
-call pandame.bat rawpal.csv pandapal.csv
-csvgrep -c county -m Bro bigfile.csv >rawbro.csv
-call pandame.bat rawbro.csv pandabro.csv
-csvgrep -c county -m Mrt bigfile.csv >rawmrt.csv
-call pandame.bat rawmrt.csv pandamrt.csv
-csvgrep -c county -m Stl bigfile.csv >rawstl.csv
-call pandame.bat rawstl.csv pandastl.csv
+start csvgrep -c county -m Pal bigfile.csv >rawpal.csv && call pandame.bat rawpal.csv pandapal.csv
+start csvgrep -c county -m Bro bigfile.csv >rawbro.csv && call pandame.bat rawbro.csv pandabro.csv
+start csvgrep -c county -m Mrt bigfile.csv >rawmrt.csv && call pandame.bat rawmrt.csv pandamrt.csv
 
-c:\pypy\pypy voterhistory.py
+csvgrep -c county -m Stl bigfile.csv >rawstl.csv && call pandame.bat rawstl.csv pandastl.csv
+
