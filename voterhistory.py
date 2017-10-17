862dedae8753faf10e981366ab4b7636bbd4ef10
Mike Stucka <stucka@whitedoggies.com>
2017-10-17 14:35:53 -0400

    Python 3.6. Capwords instead of Titlecase. Linted.

--- voterhistory.py
+++ voterhistory.py
@@ -1,42 +1,45 @@
-#!/python27/python
+#!c:/python36/python
+# #!/python27/python
 
 import csv
 import os
-import titlecase
+
+# import titlecase
+from string import capwords
 import sys
 import time
-import datetime
+# import datetime
 
-buffersize=20000            # Default is 8192
-counter=0
-starttime=time.clock()
-print "2,000 records are marked with ."
+buffersize = 20000            # Default is 8192
+counter = 0
+starttime = time.clock()
+print("2,000 records are marked with .")
 
 with open('fullhistory.csv', 'wb', buffersize) as outputfile:
     c = csv.writer(outputfile)
     c.writerow(["county", "id", "electiondate", "historycode"])
-    filelist=os.listdir("VoterHistory/")
+    filelist = os.listdir("VoterHistory/")
     for inputfile in filelist:
-        with open("VoterHistory/" + inputfile, 'rU', buffersize) as inputcsv:        
+        with open("VoterHistory/" + inputfile, 'rU', buffersize) as inputcsv:
             rows = csv.reader(inputcsv, delimiter='\t')
             for row in rows:
-                row[0]=titlecase.titlecase(row[0])
-                if row[2].count("/")==2:
-                    mydate=row[2].split("/")
-                    row[2]=mydate[2] + "-" + mydate[0] + "-" + mydate[1]
-                elif len(row[2])==0:
-                    row[2]="0000-00-00"
+                row[0] = capwords(row[0])
+                if row[2].count("/") == 2:
+                    mydate = row[2].split("/")
+                    row[2] = mydate[2] + "-" + mydate[0] + "-" + mydate[1]
+                elif len(row[2]) == 0:
+                    row[2] = "0000-00-00"
                 c.writerow(row)
-                counter +=1
-                if counter%100000==0:
+                counter += 1
+                if counter % 100000 == 0:
                     sys.stdout.write('!' + '\r\n')
-                    endtime=time.clock()
-                    print str("{:,}".format(counter)) + " records in " + str(endtime-starttime) + " seconds."
-                elif counter%2000==0:
+                    endtime = time.clock()
+                    print(str("{:,}".format(counter)) + " records in " + str(endtime-starttime) + " seconds.")
+                elif counter % 2000 == 0:
                     sys.stdout.write('.')
                 else:
                     pass
-print ""
-endtime=time.clock()
-print "Processed " + str("{:,}".format(counter)) + " records in " + str(endtime-starttime) + " seconds (" + str((endtime-starttime)/60) + " minutes.)"
-print str(counter / (endtime-starttime)) + " records per second."
+print("")
+endtime = time.clock()
+print("Processed " + str("{:,}".format(counter)) + " records in " + str(endtime-starttime) + " seconds (" + str((endtime-starttime)/60) + " minutes.)")
+print(str(counter / (endtime-starttime)) + " records per second.")
